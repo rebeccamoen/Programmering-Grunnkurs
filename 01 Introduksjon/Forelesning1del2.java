1d9cd9f5d8aef24438b75753bc401df30ca56a9b
rebeccamoen <54799857+rebeccamoen@users.noreply.github.com>
2020-09-06 18:06:07 +0200

    Add files via upload

---
+++ 01 Introduksjon/Forelesning1del2.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Forelesning1del2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dette er mitt første program");
+		
+		JOptionPane.showMessageDialog(null, "Jeg heter Morten");
+		JOptionPane.showInputDialog(null, "Hvem er sterkest, Batman eller Robin?");
+		
+	}
+
+}
